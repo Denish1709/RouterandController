e19972ae5bc907666a4cdaf7aed82c7309689a87
Denish1709 <denishrao22s@forward.edu.my>
2023-08-20 23:39:21 +0800

    ExpressJS 2

--- server.js
+++ server.js
@@ -1,79 +1,72 @@
 const express = require("express");
 const app = express();
 
-let books = [
+let parks = [
   {
     id: 1,
-    title: "Book One",
-    description: "Description of book one",
-    authorId: 1,
+    name: "Yellowstone National Park",
+    facilities: ["campgrounds", "visitor-center", "trails"],
   },
   {
     id: 2,
-    title: "Book Two",
-    description: "Description of book two",
-    authorId: 2,
+    name: "Zion National Park",
+    facilities: ["trails", "visitor-center"],
   },
 ];
 
-let reviews = [
-  { id: 1, text: "Amazing book!", bookId: 1 },
-  { id: 2, text: "Decent read.", bookId: 2 },
+let visitors = [
+  { id: 1, name: "John Doe", pastReservations: [1], upcomingReservations: [2] },
+  { id: 2, name: "Jane Smith", pastReservations: [], upcomingReservations: [] },
 ];
 
-let authors = [
-  { id: 1, name: "Author One", bio: "Bio of Author One" },
-  { id: 2, name: "Author Two", bio: "Bio of Author Two" },
+let reservations = [
+  { id: 1, parkId: 1, visitorId: 1, date: "2023-09-01" },
+  { id: 2, parkId: 2, visitorId: 1, date: "2023-10-01" },
 ];
 
-app.get("/books", (req, res) => {
-  res.json(books);
+app.get("/parks", (req, res) => {
+  res.json(parks);
 });
-app.get("/books/:id", (req, res) => {
-  const book = books.find((p) => parseInt(p.id) === parseInt(req.params.id));
-  if (book) {
-    const author = authors.find((author) => author.id === book.authorId);
-    res.status(200).json({ ...book, name: author.name, bio: author.bio });
+
+app.get("/parks/:id", (req, res) => {
+  const park = parks.find((p) => parseInt(p.id) === parseInt(req.params.id));
+  if (park) {
+    res.status(200).json(park);
   } else {
-    res.status(400).json("ID provided is not available");
+    // error handling
+    res.status(404).json({ error: "Park ID provided is not available" });
   }
 });
-app.get("/reviews", (req, res) => {
-  res.json(reviews);
+
+app.get("/visitors", (req, res) => {
+  res.json(visitors);
 });
-app.get("/reviews/:id", (req, res) => {
-  const review = reviews.find(
+
+app.get("/visitors/:id", (req, res) => {
+  const visitorId = parseInt(req.params.id);
+});
+
+app.get("/reservations", (req, res) => {
+  res.json(reservations);
+});
+
+app.get("/reservations/:id", (req, res) => {
+  const reservation = reservations.find(
     (p) => parseInt(p.id) === parseInt(req.params.id)
   );
-  if (review) {
-    const book = books.find((b) => b.id === review.bookId);
-    res.status(200).json({ ...review, book_title: book.title });
+  if (reservation) {
+    res.status(200).json(reservation);
   } else {
-    res.status(400).json("ID provided is not available");
-  }
-});
-app.get("/authors", (req, res) => {
-  res.json(authors);
-});
-app.get("/authors/:id", (req, res) => {
-  const author = authors.find(
-    (p) => parseInt(p.id) === parseInt(req.params.id)
-  );
-  if (author) {
-    res.status(200).json(author);
-  } else {
-    res.status(400).json("ID provided is not available");
+    res.status(404).json("Reservation ID is not available");
   }
 });
 
 app.get("/", (req, res) => {
   res.send(
-    '<a href="/books">Books</a> <a href="/reviews">Reviews</a> <a href="/authors">Authors</a>'
+    '<a href="/parks"> Parks </a> <br/> <a href="/visitors"> Visitors </a> <br/> <a href="/reservations"> Reservation </a>'
   );
 });
 
-// Your routing and controller code goes here
-
 app.listen(5000, () => {
-  console.log("Bookstore app is running on port 3000");
+  console.log("National Park Visitor System is running on port 5000");
 });
